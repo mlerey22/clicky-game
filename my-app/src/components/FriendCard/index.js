7d2f5d51c52d195cd3bae715816818c1595578a5
Cash <56851337+mlerey22@users.noreply.github.com>
2020-05-18 17:12:21 -0500

    trying to get props to work...not working so far

--- my-app/src/components/FriendCard/index.js
+++ my-app/src/components/FriendCard/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./style.css";
 
-class FriendCard extends React.Component(props) {
+class FriendCard extends Component {
   // Setting the initial state of the Counter component
-  state = {
-    count: 0
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+  }
 
-  // handleIncrement increments this.state.count by 1
-  handleIncrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
-  };
 
 
   // handleIncrement increments this.state.count by 1
